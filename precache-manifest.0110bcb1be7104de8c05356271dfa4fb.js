40713a4eed2bf66ad9923a7a303516e74746b7fe
Adam Klin <adam.klin@assecobs.pl>
2020-09-28 19:08:38 +0200

    Updates

---
+++ precache-manifest.0110bcb1be7104de8c05356271dfa4fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e1403154f9268c6f4c0135cb2e3ca71",
+    "url": "/beer-api-project/index.html"
+  },
+  {
+    "revision": "a15124ed3aaaad8c5dc1",
+    "url": "/beer-api-project/static/js/2.97adb864.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/beer-api-project/static/js/2.97adb864.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a93a17b54aa97023ebe",
+    "url": "/beer-api-project/static/js/main.7622bcf1.chunk.js"
+  },
+  {
+    "revision": "3e523cce9b61373fb94e",
+    "url": "/beer-api-project/static/js/runtime-main.e00cf880.js"
+  },
+  {
+    "revision": "c613ff9a47f2e98e147e967bfc75f939",
+    "url": "/beer-api-project/static/media/noimage.c613ff9a.jpg"
+  }
+]);
